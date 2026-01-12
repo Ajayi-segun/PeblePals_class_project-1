23abbca3eaacbb243565411f7ce90e52175b631e
Maciej-Clanet <maciejclanet@gmail.com>
2026-01-12 09:34:53 +0000

    some prep for nav script

---
+++ scripts/nav.js
@@ -0,0 +1,33 @@
+
+
+function is_game_started() {
+    return false;
+}
+
+function is_page_active(page_name) {
+    const url_full = window.location.href;
+    const found = url_full.search(page_name.toLowerCase());
+    
+    if (url_full == "http://127.0.0.1:5500/" || url_full == "http://127.0.0.1:5500/index.html") {
+        if (page_name == "HOME") {
+            return true
+        }
+    }
+    return found > 0
+}
+
+const nav_links = [
+    { "label": "HOME", "link": "/" },
+    { "label": "ABOUT", "link": "/about.html" },
+    { "label": "TESTIMONIALS", "link": "/testimonials.html" },
+    is_game_started() ? { "label": "GAME", "link": "/game.html" } : { "label": "ADOPT", "link": "/adopt.html" }
+]
+
+let nav_container = document.getElementById("main-nav");
+
+for (let button of nav_links) {
+    nav_container.innerHTML += `<a class="nav-link 
+        ${is_page_active(button.label) ? "active" : ""}
+    "
+    href="${button.link}">${button.label}</a>`;
+}
