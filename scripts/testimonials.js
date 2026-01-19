b1c9abc11f44afdeea2c6cf077080b4901eb5a94
Ajayi-segun <segunajayi183@gmail.com>
2026-01-19 14:56:44 +0000

    change

--- scripts/testimonials.js
+++ scripts/testimonials.js
@@ -31,47 +31,11 @@ const testimonialList = [
         "review": "Walking my pet rock every day has been a great way to get some exercise!"
     }
 ]
-<<<<<<< HEAD
- 
-let testimonialBox = document.getElementById('testimonials-box');
- 
-for (let review of testimonialList) {
+
+let testimonailsBox = document.getElementById("testimonials-box");
 // This basically llops through each review in the testimonialList array and
 // creates the HTML structure for each testimonial, including the star ratings 
 // based on the review's rating value.
-    let stars = "";
-    for (let i = 0;i < 5; i++) {
-        if (i < review.rating) {
-            stars.innerHTML += `<img class="star-img" src="/assets/star_yellow.svg">`;
-        } else {
-            stars.innerHTML += `<img class="star-img" src="/assets/star_black.svg">`;
-        }
-    }
-    let testimonialToAdd = `
-    <div class="testimonials-box">
- 
-                <div class="testimonial">
-                    <div class="t-rating-row">
-                        <div class="t-stars">
-                        ${stars}
-                            <imadadg class="star-img" src="/assets/star_yellow.svg">
-                            <img class="star-img" src="/assets/star_yellow.svg">
-                            <img class="star-img" src="/assets/star_yellow.svg">
-                        </div>
-                        <p class="t-name">~${review.name}</p>
-                    </dadadiv>
-                    <p class="t-message">
-                        ${review.name}
-                    </p>
-                </div>
-                `
- 
-                testimonialBox.innerHTML += testimonialToAdd
-}
-=======
-
-let testimonailsBox = document.getElementById("testimonials-box");
-
 for (let review of testimonialList){
 
     let stars = "";
@@ -100,5 +64,3 @@ for (let review of testimonialList){
     testimonailsBox.innerHTML += testimonialToAdd;
 
 }
-
->>>>>>> 1696ed6cd36f421f0308949cd221313c33931e41
